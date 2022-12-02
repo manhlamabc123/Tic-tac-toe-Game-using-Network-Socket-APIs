2e18804e9384b9f78c146679898bd420bc978ef5
manh <tranducmanh12092001@gmail.com>
2022-12-02 21:36:34 +0700

    Create Sing in function

--- helper/helper.c
+++ helper/helper.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "helper.h"
 #include "../exception/exception.h"
+#include "../account/account.h"
 #define BUFFER_SIZE 1024
 
 int menu()
@@ -88,4 +89,60 @@ goal:
         return 1;
     else
         return 0;
+}
+
+int sign_up(int socket_fd)
+{
+
+    char username[BUFFER_SIZE];
+    char password[BUFFER_SIZE];
+    char confirm_password[BUFFER_SIZE];
+    char control_signal[BUFFER_SIZE] = "sign in\0"; // for now
+
+    printf("---------Sign up-----------\n");
+goal:
+    printf("Username: ");
+    if (fgets(username, sizeof(username), stdin) == NULL)
+    {
+        printf("Input error.\n");
+        return 0;
+    }
+    if (check_spaces(username, sizeof(username)))
+    {
+        printf("Contain white scape(s). Try again.\n");
+        goto goal;
+    }
+goal1:
+    printf("Password: ");
+    if (fgets(password, sizeof(password), stdin) == NULL)
+    {
+        printf("Input error.\n");
+        return 0;
+    }
+    if (check_spaces(password, sizeof(password)))
+    {
+        printf("Contain white scape(s). Try again.\n");
+        goto goal1;
+    }
+goal2:
+    printf("New password: ");
+    if (fgets(confirm_password, sizeof(confirm_password), stdin) == NULL)
+    {
+        printf("Input error.\n");
+        return 0;
+    }
+    if (check_spaces(confirm_password, sizeof(confirm_password)))
+    {
+        printf("Contain white scape(s). Try again.\n");
+        goto goal2;
+    }
+    if (check_confirm_password(confirm_password, password))
+    {
+        goto goal2;
+    }
+
+    send(socket_fd, control_signal, sizeof(control_signal), 0);
+    send(socket_fd, username, sizeof(username), 0);
+    send(socket_fd, confirm_password, sizeof(confirm_password), 0);
+    return 1;
 }
--- helper/helper.h
+++ helper/helper.h
@@ -1,3 +1,4 @@
 char welcome();
 int menu();
 int exit();
+int sign_up(int socket_fd);
