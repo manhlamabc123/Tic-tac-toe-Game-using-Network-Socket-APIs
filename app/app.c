476e86a00b97dc9bb9e53c1de8cc909f2053542d
manh <tranducmanh12092001@gmail.com>
2022-12-18 19:42:04 +0700

    Save

--- app/app.c
+++ app/app.c
@@ -29,21 +29,23 @@ goal:
 		switch (atoi(sign_in_feedback))
 		{
 		case 0:
-			printf("Signed in successfully.\n");
+			printf("[-]Signed in successfully.\n");
 			is_signed_in = 1;
-			break;
+			goto goal;
 		case 1:
-			printf("Cannot find account.\n");
-			return;
+			printf("[-]Cannot find account.\n");
+			goto goal;
 		case 2:
-			printf("Wrong password.\n");
-			return;
+			printf("[-]Wrong password.\n");
+			goto goal;
 		default:
-			return;
+			printf("[-]Program error.\n");
+			goto goal;
 		}
 		break;
 	case 'n':
 		sign_up(socket_fd);
+		goto goal;
 		break;
 	case 'b':
 		if (program_exit(socket_fd))
@@ -83,32 +85,36 @@ void *server_app(void *arg)
 	char client_signal[BUFFER_SIZE];
 	Account *account = NULL;
 	account = read_account(account);
+	pthread_t thread_id = pthread_self();
 
-	bzero(client_signal, sizeof(client_signal));
-	if (recv(client_fd, client_signal, sizeof(client_signal), 0) < 0)
+	while (1)
 	{
-		printf("[-]Fail to receive client message.\n");
-	}
-	else
-	{
-		standardize_input(client_signal, sizeof(client_signal));
-		printf("[+]Client message: %s\n", client_signal);
-	}
+		if (recv(client_fd, client_signal, sizeof(client_signal), 0) < 0)
+		{
+			printf("[-]Fail to receive client message.\n");
+		}
+		else
+		{
+			standardize_input(client_signal, sizeof(client_signal));
+			printf("[+]Client message: %s\n", client_signal);
 
-	switch (atoi(client_signal))
-	{
-	case 0: // exit
-		return NULL;
-	case 1: // sign up
-		printf("[+]Client trying to sign up.\n");
-		account = account_sign_up(client_fd, account);
-		return NULL;
-	case 2: // sign in
-		printf("[+]Client trying to sign in.\n");
-		account_sign_in(client_fd, account);
-		return NULL;
-	default:
-		printf("[-]Server don't understand this signal.\n");
-		return NULL;
+			switch (atoi(client_signal))
+			{
+			case 0: // exit
+				printf("[+]Closing thread: %ld.\n", thread_id);
+				return NULL;
+			case 1: // sign up
+				printf("[+]Client trying to sign up.\n");
+				account = account_sign_up(client_fd, account);
+				break;
+			case 2: // sign in
+				printf("[+]Client trying to sign in.\n");
+				account_sign_in(client_fd, account);
+				break;
+			default:
+				printf("[-]Server don't understand this signal.\n");
+				break;
+			}
+		}
 	}
 }
