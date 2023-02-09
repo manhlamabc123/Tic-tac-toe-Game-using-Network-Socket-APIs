c02ebd96206ea49a172c49cf6ee47ce6e9b7cea6
manh <tranducmanh12092001@gmail.com>
2023-02-09 17:51:49 +0700

    Fix bug: No user for logout

--- server_side/server.c
+++ server_side/server.c
@@ -202,8 +202,9 @@ int main(int argc, char *argv[])
                     print_rooms(rooms);
                 }
 
-                Account* current_user = account_search_by_socketfd(acc, ufds[i].fd);
-                account_log_out(ufds[i].fd, acc, *current_user);
+                Account *current_user = account_search_by_socketfd(acc, ufds[i].fd);
+                if (current_user != NULL)
+                    account_log_out(ufds[i].fd, acc, *current_user);
 
                 close(ufds[i].fd);
                 ufds[i].fd = -1;
